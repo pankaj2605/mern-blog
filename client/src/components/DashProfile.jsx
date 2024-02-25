bdf87c9257de99e1e878baca9bc5364b4816b6f9
Pankaj Kumar <pankajs2167@gmal.com>
2024-02-25 13:06:32 +0530

    complete upload post image functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -80,6 +80,7 @@ export default function DashProfile() {
                         }
                       })
                       setImageFileUploading(false);
+                      setImageFileUploadProgress(null)
                 })
             }
         );

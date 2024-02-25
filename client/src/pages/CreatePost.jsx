dbbde463f2f7fd6662aa49e8cfd0cbe607562e51
Pankaj Kumar <pankajs2167@gmal.com>
2024-02-25 13:38:08 +0530

    complete create post functionallity

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,15 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { app } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreatePost() {
     const[file,setFile]=useState(null);
     const[imageUploadProgress,setImageUploadProgress]=useState(null);
     const[imageUploadError,setImageUploadError]=useState(null);
     const [formData,setFormData]=useState({});
+    const [publishError,setPublishError] =useState(null);
+    const navigate=useNavigate();
     const handleUploadImage=async()=>{
         try{
             if(!file){
@@ -57,13 +60,62 @@ export default function CreatePost() {
             setImageUploadProgress(null);
         }
     }
+    const handleInputText=(event)=>{
+        setFormData((formData) => {
+            return {
+              ...formData,                  
+              title: event.target.value,
+            }
+          })
+    }
+    const handleInputSelect=(event)=>{
+        setFormData((formData) => {
+            return {
+              ...formData,                  
+              category: event.target.value,
+            }
+          })
+    }
+    const handleInputContent=(value)=>{
+        setFormData((formData) => {
+            return {
+              ...formData,                  
+              content: value,
+            }
+          })
+    }
+    const handleSubmit= async (event)=>{
+        event.preventDefault();
+        try{
+            const res =await fetch('api/post/create',{
+                method:'POST',
+                headers:{'Content-Type':'application/json'},
+                body:JSON.stringify(formData)
+              });
+              const data=await res.json();
+              if(!res.ok){
+                setPublishError(data.message)
+                return
+              }
+            //   if(data.success === false){
+            //     setPublishError(data.message)
+            //     return
+            //   }
+              if(res.ok){
+                setPublishError(null)
+                navigate(`/post/${data.slug}`)
+              }
+        }catch(error){
+            setPublishError('Something went wrong')
+        }
+    }
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-        <form className='flex flex-col gap-4 '>
+        <form className='flex flex-col gap-4 ' onSubmit={handleSubmit}>
             <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-                <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
-                <Select>
+                <TextInput type='text' placeholder='Title' required id='title' className='flex-1' onChange={handleInputText}/>
+                <Select onChange={handleInputSelect}>
                     <option value="uncategorized">Select a category</option>
                     <option value="javascript">JavaScript</option>
                     <option value="reactjs">React.js</option>
@@ -91,8 +143,9 @@ export default function CreatePost() {
             </div>
             {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
             {formData.image && <img src={formData.image} alt='upload' className='w-full h-72 object-cover'/>}
-            <ReactQuill theme="snow" placeholder='write something...' className='h-72 mb-12' required/>
+            <ReactQuill theme="snow" placeholder='write something...' className='h-72 mb-12' required onChange={handleInputContent}/>
             <Button type='submit' gradientDuoTone='purpleToPink'>Publish</Button>
+            {publishError && <Alert color='failure'>{publishError}</Alert>}
         </form>
     </div>
   )

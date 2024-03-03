c505e42e9eb80238bf856f1118cd8321d00a06f4
Pankaj Kumar <pankajs2167@gmal.com>
2024-03-03 13:53:23 +0530

    add delete functionallity to the comment section

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -82,3 +82,23 @@ export const editComment =async (req,res,next) =>{
         next(error);
     }
 }
+
+
+
+export const deleteComment =async (req,res,next) =>{
+    try{
+        const comment =await Comment.findById(req.params.commentId);
+        if(!comment){
+            return next(errorHandler(404,'Comment not found'))
+        }
+
+        if(req.user.id !== comment.userId && !req.user.isAdmin){
+            return next(errorHandler(403,'You are not allowed to delete this comment'))
+        }
+
+        await Comment.findByIdAndDelete(req.params.commentId);
+        res.status(200).json({message:"Comment deleted successfully"});
+    }catch(error){
+        next(error);
+    }
+}
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import {FaThumbsUp} from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
-export default function Comment({comment,onLike,onEdit}) {
+export default function Comment({comment,onLike,onEdit,onDelete}) {
     const [user,setUser]=useState(null);
     const {currentUser} = useSelector((state)=>state.user);
     const [isEditing,setIsEditing]=useState(false);
@@ -84,7 +84,11 @@ export default function Comment({comment,onLike,onEdit}) {
                             </p>
                             {
                                 currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                                    <button type='button' className='text-gray-400 hover:text-blue-500' onClick={handleEdit}>Edit</button>
+                                    <>
+                                        <button type='button' className='text-gray-400 hover:text-blue-500' onClick={handleEdit}>Edit</button>
+                                        <button type='button' className='text-gray-400 hover:text-red-500' onClick={()=>onDelete(comment._id)}>Delete</button>
+                                    </>
+                                    
                                 )
 
                                 
